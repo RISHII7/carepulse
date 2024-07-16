57aa7fdb0174062889223aeeb6aeb9be925d8466
RISHII7 <rishikesh.palande07@gmail.com>
2024-07-16 13:28:07 +0530

    Pending

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -25,7 +26,7 @@ export enum FormFieldType {
 const PatientForm = () => {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false)
-    
+
     const form = useForm<z.infer<typeof UserFormValidation>>({
         resolver: zodResolver(UserFormValidation),
         defaultValues: {
@@ -35,18 +36,27 @@ const PatientForm = () => {
         },
     })
 
-    async function onSubmit({ name, email, phone}: z.infer<typeof UserFormValidation>) {
+    const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
         setIsLoading(true);
 
         try {
-            // const userData = { name, email, phone};
-            // const user = await createUser(userData);
+            const user = {
+                name: values.name,
+                email: values.email,
+                phone: values.phone,
+            };
 
-            // if(user) router.push(`/patients/${user.$id}/register`)
+            const newUser = await createUser(user);
+
+            if (newUser) {
+                router.push(`/patients/${newUser.$id}/register`);
+            }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+
+        // setIsLoading(false);
+    };
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
@@ -54,33 +64,33 @@ const PatientForm = () => {
                     <h1 className="header">Hi there 👋</h1>
                     <p className="text-dark-700">Schedule your first appointment</p>
                 </section>
-                
-                <CustomFormField 
-                fieldType = {FormFieldType.INPUT} 
-                control = {form.control} 
-                name = "name"
-                label = "Full name"
-                placeholder = "John Doe"
-                iconSrc = "/assets/icons/user.svg"
-                iconAlt = "user"
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
                 />
 
-                <CustomFormField 
-                fieldType = {FormFieldType.INPUT} 
-                control = {form.control} 
-                name = "email"
-                label = "Email"
-                placeholder = "johndoe@gmail.com"
-                iconSrc = "/assets/icons/email.svg"
-                iconAlt = "email"
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="johndoe@gmail.com"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
                 />
 
-                <CustomFormField 
-                fieldType = {FormFieldType.PHONE_INPUT} 
-                control = {form.control} 
-                name = "phone"
-                label = "Phone Number"
-                placeholder = "+91 8459972812"
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="phone"
+                    label="Phone Number"
+                    placeholder="+91 8459972812"
                 />
 
                 <SubmitButton isLoading={isLoading}>
